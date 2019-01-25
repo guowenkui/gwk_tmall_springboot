bae9cfc32b534ef73e26346f97beb874ce3897ed
郭文魁 <guowenkui@rongzi.com>
2019-01-25 10:41:19 +0800

    前台-需要登录--登录状态拦截器

---
+++ src/main/java/com/how2java/tmall/interceptor/LoginInterceptor.java
@@ -0,0 +1,73 @@
+package com.how2java.tmall.interceptor;
+
+import com.how2java.tmall.pojo.User;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.apache.commons.lang.StringUtils.remove;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        User user = (User) session.getAttribute("user");
+
+        String path = httpServletRequest.getContextPath();//  /tmall_springboot
+//        String path02 = session.getServletContext().getContextPath();
+
+        String uri = httpServletRequest.getRequestURI();//  /tmall_springboot/cart
+        String page =   StringUtils.remove(uri,path+"/");//  cart
+
+        String[] requiredPages = new String[]{
+                "buy",
+                "alipay",
+                "payed",
+                "cart",
+                "bought",
+                "confirmPay",
+                "orderConfirmed",
+                "forebuyone",
+                "forebuy",
+                "foreaddCart",
+                "forecart",
+                "forechangeOrderItem",
+                "foredeleteOrderItem",
+                "forecreateOrder",
+                "forepayed",
+                "forebought",
+                "foreconfirmPay",
+                "foreorderConfirmed",
+                "foredeleteOrder",
+                "forereview",
+                "foredoreview"
+        };
+
+        for (String requiredPage: requiredPages){
+            if (page.startsWith(requiredPage)){
+                if (user==null){
+                    httpServletResponse.sendRedirect("login");
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
